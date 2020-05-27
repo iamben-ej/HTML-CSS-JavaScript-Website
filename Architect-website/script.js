679401242e0c842cdf4d063cac8ca35f01465b37
iamben-ej <ejben55@gmail.com>
2020-05-27 09:57:28 +0100

    Media query done

--- Architect-website/script.js
+++ Architect-website/script.js
@@ -2,11 +2,11 @@ document.querySelector(".hamburger-menu").addEventListener("click", () => {
     document.querySelector(".container").classList.toggle("change");
 })
 
-document.querySelector('scroll-btn').addEventListener(
-    'click', () => {
-        document.querySelector('html').style.scrollBehavior = 'smooth';
+document.querySelector("scroll-btn").addEventListener(
+    "click", () => {
+        document.querySelector("html").style.scrollBehavior = "smooth";
         setTimeout(() => {
-            document.querySelector('html').style.scrollBehavior = 'unset';
+            document.querySelector("html").style.scrollBehavior = "unset";
         }, 1000);
     }
 );
